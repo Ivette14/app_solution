20b2e9353fd9b6e376ca7d7b07e196d892d9c908
Mario Rivas <mariorivasugb@gmail.com>
2014-06-28 08:32:28 -0600

    BD Modificada

--- app_solution.sql
+++ app_solution.sql
@@ -1,32 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 4.1.12
--- http://www.phpmyadmin.net
+CREATE DATABASE  IF NOT EXISTS `app_solution` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `app_solution`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 27-06-2014 a las 10:53:35
--- Versión del servidor: 5.6.16
--- Versión de PHP: 5.5.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: app_solution
+-- ------------------------------------------------------
+-- Server version	5.6.15
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `app_solution`
+-- Table structure for table `caso`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `caso`
---
-
-CREATE TABLE IF NOT EXISTS `caso` (
+DROP TABLE IF EXISTS `caso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `caso` (
   `id_caso` int(10) NOT NULL AUTO_INCREMENT,
   `nombre_caso` varchar(30) NOT NULL,
   `reporto_caso` varchar(30) NOT NULL,
@@ -34,109 +32,206 @@ CREATE TABLE IF NOT EXISTS `caso` (
   `fecha_reporte` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `ubicacion` varchar(30) NOT NULL,
   `estado` int(2) NOT NULL,
+  `asignado` int(2) DEFAULT NULL,
   PRIMARY KEY (`id_caso`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `caso_cate`
+-- Dumping data for table `caso`
 --
 
-CREATE TABLE IF NOT EXISTS `caso_cate` (
+LOCK TABLES `caso` WRITE;
+/*!40000 ALTER TABLE `caso` DISABLE KEYS */;
+INSERT INTO `caso` VALUES (1,'caso1','deportaaaa','casp1','2014-06-28 07:23:43','adsdas',1,NULL),(2,'gallo','gasd','fga','2014-06-28 10:20:04','fasd',1,2),(3,'sdf','ghf','gsdf','2014-06-28 10:06:23','sdf',1,1),(4,'bbbb','mnnnn','nnnn','2014-06-28 07:43:06','nnn',1,1);
+/*!40000 ALTER TABLE `caso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `caso_cate`
+--
+
+DROP TABLE IF EXISTS `caso_cate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `caso_cate` (
   `numero` int(5) NOT NULL,
   `id_caso` int(10) NOT NULL,
   `id_subcategoria` int(10) NOT NULL,
   `fecha_resolucion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `categoria`
+-- Dumping data for table `caso_cate`
 --
 
-CREATE TABLE IF NOT EXISTS `categoria` (
+LOCK TABLES `caso_cate` WRITE;
+/*!40000 ALTER TABLE `caso_cate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `caso_cate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
   `id_categoria` int(10) NOT NULL AUTO_INCREMENT,
   `nombre_cate` varchar(30) NOT NULL,
   `descripcion` varchar(100) NOT NULL,
   PRIMARY KEY (`id_categoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `opcion`
+-- Dumping data for table `categoria`
 --
 
-CREATE TABLE IF NOT EXISTS `opcion` (
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `opcion`
+--
+
+DROP TABLE IF EXISTS `opcion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `opcion` (
   `id_opcion` int(10) NOT NULL AUTO_INCREMENT,
   `opcion` varchar(30) NOT NULL,
   `url` varchar(50) NOT NULL,
   `id_menu` int(10) NOT NULL,
   PRIMARY KEY (`id_opcion`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `rol`
+-- Dumping data for table `opcion`
 --
 
-CREATE TABLE IF NOT EXISTS `rol` (
+LOCK TABLES `opcion` WRITE;
+/*!40000 ALTER TABLE `opcion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `opcion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rol`
+--
+
+DROP TABLE IF EXISTS `rol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rol` (
   `id_rol` int(10) NOT NULL AUTO_INCREMENT,
   `rol` varchar(20) NOT NULL,
   `rol_menu` varchar(20) NOT NULL,
   `id_opcion` int(10) NOT NULL,
   PRIMARY KEY (`id_rol`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `seguimiento`
+-- Dumping data for table `rol`
 --
 
-CREATE TABLE IF NOT EXISTS `seguimiento` (
+LOCK TABLES `rol` WRITE;
+/*!40000 ALTER TABLE `rol` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seguimiento`
+--
+
+DROP TABLE IF EXISTS `seguimiento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `seguimiento` (
   `id_seguimiento` int(10) NOT NULL AUTO_INCREMENT,
   `id_caso` int(10) NOT NULL,
   `responsable` varchar(30) NOT NULL,
   `fecha_asignacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `causa` varchar(250) NOT NULL,
-  `conclusion` varchar(250) NOT NULL,
-  `observacion` varchar(250) NOT NULL,
+  `causa` varchar(250) DEFAULT NULL,
+  `conclusion` varchar(250) DEFAULT NULL,
+  `observacion` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id_seguimiento`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `subcategoria`
+-- Dumping data for table `seguimiento`
 --
 
-CREATE TABLE IF NOT EXISTS `subcategoria` (
+LOCK TABLES `seguimiento` WRITE;
+/*!40000 ALTER TABLE `seguimiento` DISABLE KEYS */;
+INSERT INTO `seguimiento` VALUES (1,4,'roberto','2014-06-28 10:03:53','bbbb','aaaaaa','aaaa'),(2,4,'rober','2014-06-28 10:03:53','bbbb','aaaaaa','aaaa'),(3,4,'ffff','2014-06-28 10:03:53','bbbb','aaaaaa','aaaa'),(4,2,'mario','2014-06-28 10:20:04','asdasdasd','asdasdas','asdasds'),(5,3,'yaquelin','2014-06-28 10:06:51','sdf','hjhjkhjhj','ghghghgh');
+/*!40000 ALTER TABLE `seguimiento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subcategoria`
+--
+
+DROP TABLE IF EXISTS `subcategoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subcategoria` (
   `id_subcategoria` int(10) NOT NULL AUTO_INCREMENT,
   `id_categoria` int(10) NOT NULL,
   `nombre_subcate` varchar(30) NOT NULL,
   `descripcion` varchar(100) NOT NULL,
   `ponderacion` int(1) NOT NULL,
   PRIMARY KEY (`id_subcategoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Dumping data for table `subcategoria`
 --
 
-CREATE TABLE IF NOT EXISTS `usuarios` (
+LOCK TABLES `subcategoria` WRITE;
+/*!40000 ALTER TABLE `subcategoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subcategoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
   `id_usuario` int(10) NOT NULL AUTO_INCREMENT,
   `nombre_usuario` varchar(20) NOT NULL,
   `contraseña` varchar(20) NOT NULL,
   `id_rol` int(10) NOT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-06-28  8:31:19
